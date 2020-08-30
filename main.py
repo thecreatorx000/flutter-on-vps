1a163e2c3cc2acbcebb98699e91367b63065be2f
thecreatorx000 <65585521+thecreatorx000@users.noreply.github.com>
2020-08-30 12:14:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ username = str(getpass.getuser())
 root_path = "/home/" + username + "/"
 with open(root_path + ".bashrc", "a+") as file:
     file.write("\n\n")
-    file.write('export ANDROID_HOME="/home/bkumarmahatha/Android/Sdk" \nexport PATH="$PATH:/home/bkumarmahatha/flutter/bin:$ANDROID_HOME:$ANDROID_HOME:/platform-tools"\n\n')
+    file.write('export ANDROID_HOME="' + root_path + 'Android/Sdk" \nexport PATH="$PATH:' + root_path + 'flutter/bin:$ANDROID_HOME:$ANDROID_HOME:/platform-tools"\n\n')
 
 print("#" * 10, "Setup Completed", "#" * 10)
-print("Happy Programming!")
+print("Happy Programming!\n\n")
