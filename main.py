744b3653e99f1ed8747c9b6ca391e66737795334
thecreatorx000 <65585521+thecreatorx000@users.noreply.github.com>
2020-08-30 16:24:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ with open(root_path + ".bashrc", "a+") as file:
     file.write('export ANDROID_HOME="' + root_path + 'Android/Sdk" \nexport PATH="$PATH:' + root_path + 'flutter/bin:$ANDROID_HOME:$ANDROID_HOME:/platform-tools"\n\n')
 
 print("#" * 10, "Setup Completed", "#" * 10)
-print("Happy Programming!\n\n")
+print("Happy Programming!")
+print("\nCOMMANDS\n1. $ flutter create myapp\n2. $ flutter run -d web-server --release\n3. $ flutter build apk --release\n\n")
